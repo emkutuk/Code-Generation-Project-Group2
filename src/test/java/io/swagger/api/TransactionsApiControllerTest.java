041b705f4e644a437b82fa7432e08c61b91d4e73
Stephen Holden <25555894+Sholden8340@users.noreply.github.com>
2021-06-27 20:23:55 +0200

    Add TransactionsApiControllerTest

--- src/test/java/io/swagger/api/TransactionsApiControllerTest.java
+++ src/test/java/io/swagger/api/TransactionsApiControllerTest.java
@@ -1,54 +1,249 @@
 package io.swagger.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.model.*;
+import io.swagger.security.Role;
 import io.swagger.service.TransactionService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class TransactionsApiControllerTest
-{
+@AutoConfigureMockMvc
+class TransactionsApiControllerTest {
 
-  @Autowired
-  TransactionService transactionService;
+  @Autowired ObjectMapper mapper;
+  @Autowired private MockMvc mvc;
+  @MockBean private TransactionService transactionService;
+  private User user;
+  private String customerToken;
+  private String employeeToken;
+  private List<Transaction> expectedTransactionList;
+
+  // Test Users, Accounts & Transactions
+  private Account accountToCurrent, accountFromCurrent;
+  private User customerTo, customerFrom, employee;
+
+  // Performed by employee by default
+  private RegularTransaction transactionCurrentToCurrent;
 
   @BeforeEach
-  public void init(){
-    //transactionService = new TransactionService();
+  public void setup() throws Exception {
+    mapper = new ObjectMapper();
+    mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+
+    user =
+        new User(
+            "Hein",
+            "Eken",
+            "31685032148",
+            "customer",
+            "customer",
+            new ArrayList<>(),
+            io.swagger.security.Role.ROLE_CUSTOMER,
+            AccountStatus.ACTIVE);
+    LoginDto customer = new LoginDto("customer", "customer");
+    LoginDto employee = new LoginDto("employee", "employee");
+
+    accountSetup();
+    userSetup();
+    transactionSetup();
+
+    customerToken =
+        this.mvc
+            .perform(
+                post("/Login")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(mapper.writeValueAsString(customer)))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+    employeeToken =
+        this.mvc
+            .perform(
+                post("/Login")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(mapper.writeValueAsString(employee)))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+  }
+
+  private void accountSetup() {
+    accountToCurrent = new Account("accountToCurrent", AccountType.CURRENT, 1000d);
+    accountFromCurrent = new Account("accountFromCurrent", AccountType.CURRENT, 1000d);
+  }
+
+  private void userSetup() {
+    customerTo =
+        new User(
+            "Customer",
+            "To",
+            "12345",
+            "customerTo",
+            "customerTo",
+            new ArrayList<Account>() {
+              {
+                add(accountToCurrent);
+              }
+            },
+            Role.ROLE_CUSTOMER,
+            AccountStatus.ACTIVE);
+    customerFrom =
+        new User(
+            "Customer",
+            "From",
+            "12345",
+            "customerFrom",
+            "customerFrom",
+            new ArrayList<Account>() {
+              {
+                add(accountFromCurrent);
+              }
+            },
+            Role.ROLE_CUSTOMER,
+            AccountStatus.ACTIVE);
+    employee =
+        new User(
+            "Employee",
+            "Employee",
+            "12345",
+            "Employee",
+            "Employee",
+            null,
+            Role.ROLE_EMPLOYEE,
+            AccountStatus.ACTIVE);
+  }
+
+  private void transactionSetup() {
+    transactionCurrentToCurrent =
+        new RegularTransaction(
+            accountToCurrent.getIban(),
+            accountFromCurrent.getIban(),
+            200d,
+            employee.getId(),
+            LocalDateTime.now());
+
+    expectedTransactionList =
+        new ArrayList<Transaction>() {
+          {
+            add(transactionCurrentToCurrent);
+          }
+        };
   }
 
   @Test
-  void createTransaction() {
+  @DisplayName("Create transaction should create new transaction and return created transaction")
+  void createTransaction() throws Exception {
+    given(transactionService.createTransaction(transactionCurrentToCurrent, user))
+        .willReturn(transactionCurrentToCurrent);
+
+    // writes out full date time as object instead of using the format
+    this.mvc
+        .perform(
+            post("/Transactions")
+                .header("authorization", "Bearer " + employeeToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(mapper.writeValueAsString(transactionCurrentToCurrent)))
+        .andExpect(status().isCreated());
   }
 
   @Test
-  void getTransactionsByUser() {
+  @DisplayName("Create transaction with invalid format returns Bad request")
+  void createTransactionInvalidFormat() throws Exception {
+    given(transactionService.createTransaction(transactionCurrentToCurrent, user))
+        .willReturn(transactionCurrentToCurrent);
+
+    this.mvc
+        .perform(
+            post("/Transactions")
+                .header("authorization", "Bearer " + employeeToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content("ahhh"))
+        .andExpect(status().isBadRequest());
   }
 
   @Test
-  void deleteTransactionById() {
+  @DisplayName("Get all transactions of current user or all users")
+  void getTransactionsByUser() throws Exception {
+    this.mvc
+        .perform(get("/Transactions/").header("authorization", "Bearer " + employeeToken))
+        .andExpect(status().isOk());
   }
 
   @Test
-  void depositMoney() {
+  void deleteTransactionById() throws Exception {
+    this.mvc
+        .perform(
+            delete("/Transactions/" + transactionCurrentToCurrent.getTransactionId())
+                .header("authorization", "Bearer " + employeeToken))
+        .andExpect(status().isOk());
   }
 
   @Test
-  void editTransactionById() {
+  void depositMoney() throws Exception {
+    this.mvc
+        .perform(
+            post("/Transactions/Deposit")
+                .header("authorization", "Bearer " + employeeToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(""))
+        .andExpect(status().isOk());
   }
 
   @Test
-  void getTransactionByIBAN() {
+  void editTransactionById() throws Exception {
+    this.mvc
+        .perform(
+            put("/Transactions/" + transactionCurrentToCurrent.getTransactionId())
+                .header("authorization", "Bearer " + employeeToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(mapper.writeValueAsString(transactionCurrentToCurrent.toString())))
+        .andExpect(status().isOk());
   }
 
   @Test
-  void getTransactionById() {
+  void getTransactionByIBAN() throws Exception {
+    this.mvc
+        .perform(
+            get("/Transactions")
+                .header("authorization", "Bearer " + employeeToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk());
   }
 
   @Test
-  void withdrawMoney() {
+  void getTransactionById() throws Exception {
+    this.mvc
+        .perform(
+            get("/Transactions")
+                .header("authorization", "Bearer " + employeeToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  void withdrawMoney() throws Exception {
+    this.mvc
+        .perform(
+            get("/Transactions/Withdraw")
+                .header("authorization", "Bearer " + employeeToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk());
   }
 }
