5f1bab562884c2093f86b0f6d69fabf61b528f58
Stephen Holden <25555894+Sholden8340@users.noreply.github.com>
2021-07-01 00:35:19 +0200

    Remove commented out code

--- src/main/java/io/swagger/model/RegularTransaction.java
+++ src/main/java/io/swagger/model/RegularTransaction.java
@@ -90,15 +90,4 @@ public class RegularTransaction extends Transaction {
   public String getAccountFrom() {
     return accountFrom;
   }
-
-//  Can probably remove this
-//  /**
-//   * Convert the given object to string with each line indented by 4 spaces (except the first line).
-//   */
-//  private String toIndentedString(java.lang.Object o) {
-//    if (o == null) {
-//      return "null";
-//    }
-//    return o.toString().replace("\n", "\n    ");
-//  }
 }
